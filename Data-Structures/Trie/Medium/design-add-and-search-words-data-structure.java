7e9fd573fac52c418d8bc2ac25766c5f0df47d96
KunalPV <kpv904@gmail.com>
2025-01-06 13:06:56 +0530

    updated trie/design-add-and-search-words-data-structure

--- Data-Structures/Trie/Medium/design-add-and-search-words-data-structure.java
+++ Data-Structures/Trie/Medium/design-add-and-search-words-data-structure.java
@@ -1,6 +1,11 @@
 
 // String, Depth-First Search, Design, Trie
 
+// create a children array with 26 size and a bool val eow and instantiate it with the constructor
+// in addWord func, create a curr variable with WordDictionary as its datatype, loop through the given word, if the character of the word is not in the curr.children[c - 'a'] then instantiate a new WordDictionary class to that index. set curr = curr.children[c - 'a'] and at last if it is at the last character from the word
+// in searchWord func, create a curr variable with WordDictionary as its datatype, through the given word, if c == '.' then run a nested loop inside (WordDictionary ch : curr.children), check if ch != null and ch(word.substring(i + 1)), then return true, else false.
+// check if curr.children[c - 'a'] == null and return false, update curr to curr.children[c - 'a'], and finally at the end return curr != null && curr.eow
+
 class WordDictionary {
   private WordDictionary[] children;
   boolean eow;
