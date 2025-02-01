f270dd9d0d1862b79355d38329ed544eeab052c2
KunalPV <kpv904@gmail.com>
2025-02-01 13:36:30 +0530

    updated string/maximum-consecutive-repeating-character

--- Data_Structures/String/Easy/maximum-consecutive-repeating-character.java
+++ Data_Structures/String/Easy/maximum-consecutive-repeating-character.java
@@ -1,61 +1,41 @@
+
+// String
+
+// create count, currCount and result as 0, 0 and s.charAt(0), 
+// loop 0 till n, check if i < n - 1 and s[i] == s[i + 1], if yes then increment currCount,
+// else, check if currCount > count, if yes then set count = currCount and result = s[i],
+// and set currCount to 1,
+// return result;
+
 package Data_Structures.String.Easy;
 
-import java.util.*;
-
-// Good Solution, but can be optimized in terms of space.
-// Time: O(n), n [s.toCharArray()] + n [hashmap, worst case: all unique elems in s] -> n, n = s.length()
-// Space: O(n), worst case: all unique elems, n = HashMap length
+// Optimal Solution
+// Time: O(n), each char is visited once in s, n = s.length()
+// Space: O(1), count + currCount + result + n -> 1, constant time
 class Solution1 {
-  public int maxRepeating(String s) {
+  public char maxRepeating(String s) {
     int n = s.length();
 
-    if (n == 0 || n == 1) {
-      return n;
+    if (n == 1) {
+      return s.charAt(n);
     }
 
-    int maxRepeat = 0;
+    int count = 0, currCount = 0;
+    char result = s.charAt(0);
+    for (int i = 0; i < n; i++) {
+      if (i < n - 1 && s.charAt(i) == s.charAt(i + 1)) {
+        currCount++;
+      } else {
 
-    Map<Character, Integer> map = new HashMap<>();
+        if (currCount > count) {
+          count = currCount;
+          result = s.charAt(i);
+        }
 
-    for (char c : s.toCharArray()) {
-      map.put(c, map.getOrDefault(c, 0) + 1);
-    }
-
-    for (Map.Entry<Character, Integer> e : map.entrySet()) {
-      if (e.getValue() > maxRepeat) {
-        maxRepeat = e.getValue();
+        currCount = 1;
       }
     }
 
-    return maxRepeat;
-  }
-}
-
-class Solution2 {
-  private static final int MAX_CHAR = 26;
-
-  public int maxRepeating(String s) {
-    int n = s.length();
-
-    if (n == 0 || n == 1) {
-      return n;
-    }
-
-    int maxRepeat = 0;
-    int vis[] = new int[MAX_CHAR];
-
-    Arrays.fill(vis, 0);
-
-    for (char c : s.toCharArray()) {
-      vis[c - 'a']++;
-    }
-
-    for (int num : vis) {
-      if (num > maxRepeat) {
-        maxRepeat = num;
-      }
-    }
-
-    return maxRepeat;
+    return result;
   }
 }
