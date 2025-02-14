323f64d8e17b8a7ff8cd03e1ab28681067d14596
KunalPV <kpv904@gmail.com>
2025-02-14 12:22:07 +0530

    added linked_list/delete-the-middle-node-of-the-list

---
+++ Data_Structures/Linked_List/Medium/delete-the-middle-node-of-the-list.java
@@ -0,0 +1,43 @@
+
+class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode() {
+  }
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+// Expected Solution: Slow and Fast pointers
+// Time: O(n), n/2 [slow, fast pointers both half of list] -> n, n = list length
+// Space: O(1), 1 [prev] + 1 [slow] + 1 [fast] -> 1 [all pointers] constant time
+class Solution {
+  public ListNode deleteMiddle(ListNode head) {
+    if (head == null || head.next == null) {
+      return null;
+    }
+
+    ListNode prev = new ListNode(0);
+    prev.next = head;
+
+    ListNode slow = prev;
+    ListNode fast = head;
+
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    slow.next = slow.next.next;
+
+    return head;
+  }
+}
