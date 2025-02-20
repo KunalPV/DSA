bd297b495163a6663cb2b2130f43a473a58d386f
KunalPV <kpv904@gmail.com>
2025-02-20 12:18:02 +0530

    updated tree/delete-node-in-BST

--- Data_Structures/Tree/Medium/delete-node-in-BST.java
+++ Data_Structures/Tree/Medium/delete-node-in-BST.java
@@ -1,3 +1,19 @@
+//
+// Tree, Binary Tree, Binary Search tree, Depth First Search, Recursion
+//
+// ** check if(root == null), if yes return null;
+// check if key < root.val, if yes root.left = deleteNode(root.left, key)
+// else if key > root.val, if yes root.right = deleteNode(root.right, key)
+// else, check if root.left == null, then return root.right;
+// check if root.right == null, then return root.left;
+// create a TreeNode minNode and store findMin(root.right),
+// set root.val = minNode.val,
+// root.right = deleteNode(root.right, root.val),
+// return root;
+//
+// ** TreeNode findMin(TreeNode node),
+// loop till node.left != null, node = node.left,
+// return node;
 
 class TreeNode {
 
@@ -14,17 +30,20 @@ class TreeNode {
 
     TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
+
         this.left = left;
         this.right = right;
     }
 }
 
-// Optimal Solution
+// Expected Solution: Recursive Depth First Search
+// Time: O(h), h [find the node to delete] + h [find the minimum node in right subtree] and [delete the min node] -> 2h, h = height of tree [O(n) -> skewed tree, O(logn) -> balanced tree]
+// Space: O(h), recursive call stack in worst case: O(n) [skewed tree] and best case: O(logn) [balanced tree], h = height of tree
 class Solution {
 
     public TreeNode deleteNode(TreeNode root, int key) {
         if (root == null) {
-            return root;
+            return null;
         }
 
         if (key < root.val) {
